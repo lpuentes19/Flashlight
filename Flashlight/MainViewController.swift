351c1ab90dd514ad859cabfa2c14ed91b88aed8f
lpuentes19 <luey.puentes@gmail.com>
2017-09-12 17:56:52 -0600

    Created VC and added appropriate outlets

---
+++ Flashlight/MainViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MainViewController.swift
+//  Flashlight
+//
+//  Created by Luis Puentes on 9/12/17.
+//  Copyright © 2017 LuisPuentes. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBOutlet weak var onOffButton: UIButton!
+    @IBAction func onOffButtonTapped(_ sender: Any) {
+        
+    }
+}
