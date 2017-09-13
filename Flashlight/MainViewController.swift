d2b739ba6ae7fbe952fe3850d957bfee2dbab700
lpuentes19 <luey.puentes@gmail.com>
2017-09-12 18:20:05 -0600

    Added if statement to change the button, view, and status bar style when tapped. Ready to add AVFoundation library

--- Flashlight/MainViewController.swift
+++ Flashlight/MainViewController.swift
@@ -10,12 +10,39 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    var isOn = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return isOn ? .default : .lightContent
+    }
+    
     @IBOutlet weak var onOffButton: UIButton!
     @IBAction func onOffButtonTapped(_ sender: Any) {
-        
+        if isOn == false {
+            
+            isOn = true
+            
+            onOffButton.setTitle("OFF", for: .normal)
+            onOffButton.setTitleColor(.black, for: .normal)
+            
+            view.backgroundColor = .white
+            
+            self.setNeedsStatusBarAppearanceUpdate()
+            
+        } else {
+            
+            isOn = false
+            
+            onOffButton.setTitle("ON", for: .normal)
+            onOffButton.setTitleColor(.white, for: .normal)
+            
+            view.backgroundColor = .black
+            
+            self.setNeedsStatusBarAppearanceUpdate()
+        }
     }
 }
